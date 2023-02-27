ade157c70de0829a1f61f9eecd9a77f922a1b6be
Jenn Kalkowski <jennkalkowski@gmail.com>
2023-02-27 16:00:30 -0700

    Objective2Lab1

--- Labs/Objective2Lab1.java
+++ Labs/Objective2Lab1.java
@@ -1,6 +1,6 @@
 public class Objective2Lab1 {
   public static void main(String[] args) {
     String hello = "Hello World";
-	System.out.println("String hello");
+	System.out.println(hello);
   }
 }
