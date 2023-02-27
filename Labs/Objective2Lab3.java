fbb877242364fc8cda8f4afa925b7ec5f5b4c81e
Jenn Kalkowski <jennkalkowski@gmail.com>
2023-02-27 15:26:22 -0700

    objectives

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,7 +1,7 @@
 public class Objective2Lab3 {
-	public class void main(String[] args) {
+	public static void main(String[] args) {
 		int year = 1993;
 		String month = "January";
-		boolean weekend;
+		boolean weekend = false;
 	}
 }
