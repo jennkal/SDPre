51adc97286ec73dfda82d74bcb5d5f8d3d28c954
Jenn Kalkowski <jennkalkowski@gmail.com>
2023-02-27 15:25:55 -0700

    objectives

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,7 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jennifer Kalkowski";
+    int age = 30;
+    String state = "Nebraska";
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,7 @@
+public class Objective2Lab3 {
+	public class void main(String[] args) {
+		int year = 1993;
+		String month = "January";
+		boolean weekend;
+	}
+}
