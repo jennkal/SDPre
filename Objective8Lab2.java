3ea6d16c1adf1177d91a72b645757432c724ce04
jennkal <jennkalkowski@gmail.com>
2023-03-06 16:18:35 -0700

    adding latest

---
+++ Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		//variables.
+		int sum = 0;
+
+		//for loop, increment count and update the sum.
+		for(int i = 1; i <= 20; i+=1) {
+			sum = sum + i;
+		}
+		//print out final/total sum.
+		System.out.println(sum);
+	}
+}
---
+++ Objective8Lab3.java
@@ -0,0 +1,10 @@
+public class Objective8Lab3 {
+	public static void main(String[] args) {
+		int counter = 1;
+
+		//for loop.
+		for (int i = 1; i <= 20; i++) {
+
+		}
+	}
+}
