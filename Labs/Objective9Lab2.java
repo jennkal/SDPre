5c94eeaa2081391fd57798985948a2983f99d879
jennkal <jennkalkowski@gmail.com>
2023-03-07 11:58:33 -0700

    Objective9Lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,12 @@
+public class Objective9Lab2 {
+	public static void main(String[] args) {
+		System.out.println("In main");
+		printMessage();
+		System.out.println("In main");
+	}
+
+	public static void printMessage() {
+		//solution to print out "In method".
+		System.out.println("In method");
+	}
+}
