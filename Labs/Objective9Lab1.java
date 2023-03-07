4126da9cc49561096d001a812d5deb51571ec933
jennkal <jennkalkowski@gmail.com>
2023-03-07 11:50:55 -0700

    Objective9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+	public static void main(String[] args) {
+		//solution.
+		printName();
+	}
+
+	public static void printName() {
+		System.out.println("Hello Doug");
+	}
+}
